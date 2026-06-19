65a2852a69893c6d0794ff3fba8fc72de52c23f5
Daniil Andrushko <dan007sar@gmail.com>
2026-06-19 11:07:09 +0200

    Refaktorace frontendu. Přidání SCSS struktury, SearchService a SearchResult entity. Úprava HomePresenteru a příprava na integraci Google Search API.

--- app/Presentation/Home/HomePresenter.php
+++ app/Presentation/Home/HomePresenter.php
@@ -2,9 +2,45 @@
 
 namespace App\Presentation\Home;
 
-use Nette;
+use App\Model\Service\SearchService;
+use Nette\Application\UI\Form;
+use Nette\Application\UI\Presenter;
 
-
-final class HomePresenter extends Nette\Application\UI\Presenter
+final class HomePresenter extends Presenter
 {
+    private array $results = [];
+
+    public function __construct(
+        private SearchService $searchService
+    ) {
+        parent::__construct();
+    }
+
+    public function renderDefault(): void
+    {
+        $this->template->results = $this->results;
+    }
+
+    protected function createComponentSearchForm(): Form
+    {
+        $form = new Form;
+
+        $form->addText('keyword', 'Klíčové slovo:')
+            ->setRequired('Zadejte klíčové slovo.');
+
+        $form->addSubmit('search', 'Vyhledat');
+
+        $form->onSuccess[] = $this->searchFormSucceeded(...);
+
+        return $form;
+    }
+
+    public function searchFormSucceeded(Form $form, \stdClass $values): void
+    {
+        $this->results = $this->searchService->search(
+            $values->keyword
+        );
+
+        $this->template->results = $this->results;
+    }
 }
